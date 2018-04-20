d28a5ef2ddb56fa57db3a025cc0050bf66ec9c2e
Kenny <khpham24@gmail.com>
2018-04-20 09:25:44 -0600

    Kenny Completed

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,14 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
+  const user = new User({ username, password });
+  user.save((err, user) => {
+    if (err) return res.send(err);
+    res.json({
+      success: 'user saved :)',
+      user
+    });
+  });
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
 };
 
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -7,19 +7,38 @@ const SALT_ROUNDS = 11;
 const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
   // password: required
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    // if there is an error here you'll need to handle it by calling next(err);
+    if (err) return next(err);
+    this.password = hash;
+    // Once the password is encrypted, call next() so that your userController and create a user
+    next();
+  });
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
+  bcrypt.compare(plainTextPW, this.password, (err, isValid) => {
+    if (err) return callBack(err);
+    callBack(null, isValid);
+  });
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
 };
